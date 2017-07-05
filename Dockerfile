75081c4662e00178b04bfeceeebc70477d1d56e2
Jason Bukowski <jasonbukowski@users.noreply.github.com>
2017-07-05 17:50:51 -0400

    Added Postgres support and CalendarBlock definition

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,12 @@ FROM quay.io/chainpoint/node-base:master
 COPY package.json package-lock.json /home/node/app/
 RUN /usr/local/bin/npm install --no-optional
 
-RUN mkdir /home/node/app/endpoints
-COPY ./endpoints/*.js /home/node/app/endpoints/
+RUN mkdir /home/node/app/lib
+COPY ./lib/*.js /home/node/app/lib/
+RUN mkdir /home/node/app/lib/endpoints
+COPY ./lib/endpoints/*.js /home/node/app/lib/endpoints/
+RUN mkdir /home/node/app/lib/models
+COPY ./lib/models/*.js /home/node/app/lib/models/
 
 COPY server.js /home/node/app/
 
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-// load environment variables
-const env = require('./lib/parse-env.js')
-
 const restify = require('restify')
 const corsMiddleware = require('restify-cors-middleware')
 const apiRoot = require('./lib/endpoints/root.js')
 const apiHello = require('./lib/endpoints/hello.js')
+const calendarBlock = require('./lib/models/CalendarBlock.js')
 
-// Frequency to perform the action in milliseconds
-const INTERVAL = 2000
+// pull in variables defined in shared CalendarBlock module
+let sequelize = calendarBlock.sequelize
+let CalendarBlock = calendarBlock.CalendarBlock
 
 // RESTIFY SETUP
 // 'version' : all routes will default to this version
@@ -41,12 +40,41 @@ server.get({ path: '/hello', version: '1.0.0' }, apiHello.getV1)
 // teapot
 server.get({ path: '/', version: '1.0.0' }, apiRoot.getV1)
 
-// first fire up restify and then begin background function
-// SERVER
-server.listen(8080, () => {
-  console.log('%s listening at %s', server.name, server.url)
-  // Start the interval function
-  setInterval(() => {
-    console.log(`Interval reached at ${new Date().toISOString()}`)
-  }, INTERVAL)
-})
+// fire up restify
+function startListening () {
+  server.listen(8080, () => {
+    console.log('%s listening at %s', server.name, server.url)
+  })
+}
+
+/**
+ * Opens a storage connection
+ **/
+function openStorageConnection (callback) {
+  // Sync models to DB tables and trigger check
+  // if a new genesis block is needed.
+  sequelize.sync({ logging: false }).nodeify((err) => {
+    if (err) {
+      console.error('Cannot establish Postgres connection. Attempting in 5 seconds...')
+      setTimeout(openStorageConnection.bind(null, callback), 5 * 1000)
+    } else {
+      console.log('CalendarBlock sequelize database synchronized')
+      return callback(null, true)
+    }
+  })
+}
+
+function start () {
+  // Open storage connection and then amqp connection
+  openStorageConnection((err, result) => {
+    if (err) {
+      console.error(err)
+    } else {
+      // Init intervals and restify server
+      startListening()
+    }
+  })
+}
+
+// get the whole show started
+start()
