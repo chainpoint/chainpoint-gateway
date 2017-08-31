0a7a3ce260b518c7995825e1a76faadc465e581c
Jason Bukowski <jasonbukowski@users.noreply.github.com>
2017-08-30 21:26:31 -0400

    API listen on port defined by CHAINPOINT_NODE_PORT

--- server.js
+++ server.js
@@ -225,7 +225,7 @@ async function initPublicKeysAsync (coreConfig) {
 
 // instruct restify to begin listening for requests
 function startListening (callback) {
-  apiServer.api.listen(8080, (err) => {
+  apiServer.api.listen(env.CHAINPOINT_NODE_PORT, (err) => {
     if (err) return callback(err)
     console.log(`${apiServer.api.name} listening on port ${env.CHAINPOINT_NODE_PORT}`)
     return callback(null)
