aae4cbdb2f016e575e74bc39193b83a01b40fb58
Michael Iglesias <miglesias@tierion.com>
2018-06-29 10:59:18 -0700

    Removed unnecessary setRegistration() function invocations before the process is exiting. When the process is going to exit it should not call this function. This method call is used in cases where we want the Node UI to remain running even though the node has failed to register.

--- server.js
+++ server.js
@@ -241,9 +241,6 @@ async function registerNodeAsync (nodeURI) {
               console.error(`ERROR : Registration update failed : Exiting`)
             }
 
-            // We are no longer exiting this process. Simply set registration state to 'false' which
-            // will allow the Node UI to be operational and thus display a failed registration state to the node operator.
-            apiServer.setRegistration(false)
             process.exit(0) // Currently, node-api-service will only throw a 409 if either of the following conditions are true: 1) etheruem address is already registered, 2) public URI is already registered
           } else if (error.statusCode === 426) {
             if (error.error && error.error.code && error.error.message) {
@@ -253,7 +250,6 @@ async function registerNodeAsync (nodeURI) {
             } else {
               console.error(`ERROR : Registration update failed (min node version not met) : Exiting`)
             }
-            apiServer.setRegistration(false)
             process.exit(0)
           }
 
@@ -355,9 +351,6 @@ async function registerNodeAsync (nodeURI) {
               console.error(`ERROR : Registration`)
             }
 
-            // We are no longer exiting this process. Simply set registration state to 'false' which
-            // will allow the Node UI to be operational and thus display a failed registration state to the node operator.
-            apiServer.setRegistration(false)
             process.exit(0) // Currently, node-api-service will only throw a 409 if either of the following conditions are true: 1) etheruem address is already registered, 2) public URI is already registered
           }
 
