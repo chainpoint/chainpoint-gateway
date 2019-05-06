92b018b58227ab28dc9c54cb36ad4f41ea1e7f08
Jason Bukowski <jbukowski@gmail.com>
2019-05-06 17:06:43 -0400

    Conditionally use network 1 or 3 based on NODE_ENV setting

--- cli/scripts/2_registration.js
+++ cli/scripts/2_registration.js
@@ -1,35 +1,37 @@
+// load environment variables
+let env = require('../../lib/parse-env.js').env
+
 const fs = require('fs')
 const path = require('path')
 const ethers = require('ethers')
-const Web3 = require('web3')
 const ipToInt = require('ip-to-int')
-
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://ropsten.infura.io/v3/foobar`))
 
 let tknDefinition = require('../../artifacts/ethcontracts/TierionNetworkToken.json')
 let regDefinition = require('../../artifacts/ethcontracts/ChainpointRegistry.json')
 
 const TierionNetworkTokenABI = tknDefinition.abi
 const ChainpointRegistryABI = regDefinition.abi
-const tokenAddress = tknDefinition.networks['3'].address
-const registryAddress = regDefinition.networks['3'].address
-const chainId = 3
+const network = env.NODE_ENV === 'production' ? 'homestead' : 'ropsten'
+const chainId = network === 'homestead' ? '1' : '3'
+const tokenAddress = tknDefinition.networks[chainId].address
+const registryAddress = regDefinition.networks[chainId].address
 const privateKey = fs.readFileSync(path.resolve('/run/secrets/NODE_ETH_PRIVATE_KEY'), 'utf8')
 
 const wallet = new ethers.Wallet(privateKey)
-const tokenContract = web3.eth.Contract(TierionNetworkTokenABI, tokenAddress)
-const registryContract = web3.eth.Contract(ChainpointRegistryABI, registryAddress)
+const tokenContractInterface = new ethers.utils.Interface(TierionNetworkTokenABI)
+const regContractInterface = new ethers.utils.Interface(ChainpointRegistryABI)
 
 function approve(retryCount = 1) {
   return async function(txData) {
-    const funcSigEncoded = tokenContract.methods.approve(registryAddress, 500000000000).encodeABI()
+    let functionInfo = tokenContractInterface.functions.approve
+    let functionData = functionInfo.encode([registryAddress, 500000000000])
 
     console.log(JSON.stringify(txData), 'approve txData')
 
     const tx = {
       gasPrice: txData.gasPrice + (retryCount - 1),
       gasLimit: 185000,
-      data: funcSigEncoded,
+      data: functionData,
       to: tokenAddress,
       nonce: txData.transactionCount,
       chainId: parseInt(chainId, 10)
@@ -41,16 +43,18 @@ function approve(retryCount = 1) {
 
 function register(retryCount = 1) {
   return async function([txData, registrationParams]) {
-    const funcSigEncoded = registryContract.methods
-      .stake(ipToInt(registrationParams.NODE_PUBLIC_IP_ADDRESS).toInt(), registrationParams.NODE_ETH_REWARDS_ADDRESS)
-      .encodeABI()
+    let functionInfo = regContractInterface.functions.stake
+    let functionData = functionInfo.encode([
+      ipToInt(registrationParams.NODE_PUBLIC_IP_ADDRESS).toInt(),
+      registrationParams.NODE_ETH_REWARDS_ADDRESS
+    ])
 
     console.log(JSON.stringify(txData), 'register txData')
 
     const tx = {
       gasPrice: txData.gasPrice + (retryCount - 1),
       gasLimit: 185000,
-      data: funcSigEncoded,
+      data: functionData,
       to: registryAddress,
       nonce: txData.transactionCount,
       chainId: parseInt(chainId, 10)
--- cli/scripts/3_deregister.js
+++ cli/scripts/3_deregister.js
@@ -1,19 +1,20 @@
+// load environment variables
+let env = require('../../lib/parse-env.js').env
+
 const fs = require('fs')
 const path = require('path')
 const ethers = require('ethers')
-const Web3 = require('web3')
-
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://ropsten.infura.io/v3/foobar`))
 
 let regDefinition = require('../../artifacts/ethcontracts/ChainpointRegistry.json')
 
 const ChainpointRegistryABI = regDefinition.abi
-const registryAddress = regDefinition.networks['3'].address
-const chainId = 3
+const network = env.NODE_ENV === 'production' ? 'homestead' : 'ropsten'
+const chainId = network === 'homestead' ? '1' : '3'
+const registryAddress = regDefinition.networks[chainId].address
 const privateKey = fs.readFileSync(path.resolve('/run/secrets/NODE_ETH_PRIVATE_KEY'), 'utf8')
 
 const wallet = new ethers.Wallet(privateKey)
-const registryContract = web3.eth.Contract(ChainpointRegistryABI, registryAddress)
+const regContractInterface = new ethers.utils.Interface(ChainpointRegistryABI)
 
 function deregister(retryCount = 1) {
   return async function(txData) {
@@ -32,12 +33,13 @@ function deregister(retryCount = 1) {
       )
     }
 
-    const funcSigEncoded = registryContract.methods.unStake().encodeABI()
+    let functionInfo = regContractInterface.functions.unStake
+    let functionData = functionInfo.encode()
     console.log(JSON.stringify(txData), 'deregister txData')
     const tx = {
       gasPrice: txData.gasPrice + (retryCount - 1),
       gasLimit: 185000,
-      data: funcSigEncoded,
+      data: functionData,
       to: registryAddress,
       nonce: txData.transactionCount,
       chainId: parseInt(chainId, 10)
