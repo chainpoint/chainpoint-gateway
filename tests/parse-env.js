7f83d43a6f18874da643e99d2c8ea58d6e8f5421
Jason Bukowski <jbukowski@gmail.com>
2019-08-20 16:38:00 -0700

    Remove unneeded env vars

--- tests/parse-env.js
+++ tests/parse-env.js
@@ -8,50 +8,6 @@ const expect = require('chai').expect
 const parseEnv = require('../lib/parse-env.js')
 
 describe('Environment variables', () => {
-  describe('valETHAddress', () => {
-    it('should throw error when empty', done => {
-      expect(() => {
-        parseEnv.valETHAddress('')
-      }).to.throw('The Ethereum address is invalid')
-      done()
-    })
-    it('should throw error when null', done => {
-      expect(() => {
-        parseEnv.valETHAddress(null)
-      }).to.throw('The Ethereum address is invalid')
-      done()
-    })
-    it('should throw error when not hex', done => {
-      expect(() => {
-        parseEnv.valETHAddress('not hex')
-      }).to.throw('The Ethereum address is invalid')
-      done()
-    })
-    it('should throw error when too short', done => {
-      expect(() => {
-        parseEnv.valETHAddress('0x32Be343B94f860124dC4')
-      }).to.throw('The Ethereum address is invalid')
-      done()
-    })
-    it('should throw error when too long', done => {
-      expect(() => {
-        parseEnv.valETHAddress('0x32Be343B94f860124dC4fEe278FDCBD38C102D8832Be343B94f860124dC4fEe278FDCBD38C102D88')
-      }).to.throw('The Ethereum address is invalid')
-      done()
-    })
-    it('should throw error when missing 0x prefix', done => {
-      expect(() => {
-        parseEnv.valETHAddress('32Be343B94f860124dC4fEe278FDCBD38C102D88')
-      }).to.throw('The Ethereum address is invalid')
-      done()
-    })
-    it('should return lowercased when valid', done => {
-      let result = parseEnv.valETHAddress('0x32Be343B94f860124dC4fEe278FDCBD38C102D88')
-      expect(result).to.equal('0x32be343b94f860124dc4fee278fdcbd38c102d88')
-      done()
-    })
-  })
-
   describe('valCoreIPList', () => {
     it('should return success with empty string', done => {
       let result = parseEnv.valCoreIPList('')
