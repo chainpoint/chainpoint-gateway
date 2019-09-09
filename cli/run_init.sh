e22ed677ff04deeafd2b5b069cd6f8b26db61141
Michael Iglesias <miglesias@tierion.com>
2019-09-09 13:15:55 -0700

    making changes to bash scripts entrypoints

--- cli/run_init.sh
+++ cli/run_init.sh
@@ -2,4 +2,4 @@
 cd $(dirname $0)
 source scripts/env_secrets_expand.sh
 yarn
-node cli
+node cli/swarm_secrets_init.js NODE_PUBLIC_IP_ADDRESS=${NODE_PUBLIC_IP_ADDRESS} NETWORK=${NETWORK}
--- cli/run_start_registration.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd $(dirname $0)
-source scripts/env_secrets_expand.sh
-node register.js
