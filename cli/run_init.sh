4876e21ae57f6f8ebed6abd1f1aac9eb8a10fe33
Michael Iglesias <miglesias@tierion.com>
2019-09-10 15:53:21 -0700

    small tweaks to run_init.sh

--- cli/run_init.sh
+++ cli/run_init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd $(dirname $0)
-source scripts/env_secrets_expand.sh
+# source scripts/env_secrets_expand.sh
 yarn
-node cli/swarm_secrets_init.js NODE_PUBLIC_IP_ADDRESS=${NODE_PUBLIC_IP_ADDRESS} NETWORK=${NETWORK}
+# NODE_PUBLIC_IP_ADDRESS=${NODE_PUBLIC_IP_ADDRESS} NETWORK=${NETWORK}
+node ./swarm_secrets_init.js 
