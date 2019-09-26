0ef89c56197035b1653b197d60188114f0f1eff2
Jason Bukowski <jbukowski@gmail.com>
2019-09-26 10:05:08 -0400

    Clean up LND env variables

--- cli/payment_channel_utils.js
+++ cli/payment_channel_utils.js
@@ -1,10 +1,25 @@
+/**
+ * Copyright 2019 Tierion
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// load environment variables
+const env = require('../lib/parse-env').env
+
 const lnService = require('ln-service')
 const lightning = require('lnrpc-node-client')
 const { find } = require('lodash')
 const homedir = require('os').homedir()
 const utils = require('../lib/utils')
 const commandLineArgs = require('command-line-args')
-const env = require('../lib/parse-env').env
 
 const args = process.argv.slice(2)
 
